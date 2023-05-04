9292aec1c99efe47158858e3776578fc955b2d73
Mukhammadyusuf Abdurakhimov <papa@uwussi.moe>
2023-05-03 22:51:11 -0700

    404-Image darkmode :key:

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,21 @@
 import { type NextPage } from "next";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import React from "react";
 
 const PageNotFound: NextPage = () => {
+  const { theme, systemTheme } = useTheme();
+  const isDark =
+    theme == "dark" ? true : theme == "system" ? systemTheme == "dark" : false;
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-200 dark:bg-zinc-950">
-      <Image src="/404.svg" width={512} height={512} alt="Page Not Found" />
+      <Image
+        src={isDark ? "/dark_404.svg" : "/light_404.svg"}
+        width={512}
+        height={512}
+        alt="Page Not Found"
+      />
     </div>
   );
 };
